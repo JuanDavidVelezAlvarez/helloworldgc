e28ecc0e0741072fa504228ce6710d77bf093755
Juan David Vélez CTR <juan-ctr@contiem.com>
2024-05-21 09:26:15 -0500

    fix pom repository and add the code

--- HelloWorld/src/main/java/org/example/App.java
+++ HelloWorld/src/main/java/org/example/App.java
@@ -4,10 +4,18 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+public class App implements HttpFunction {
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8));
+        writer.write("Hello, World!");
+        writer.flush();
     }
 }
